6342454babcddf6617f8fdfa5f74120d8bda83e1
Dmannen03 <39675750+Dmannen03@users.noreply.github.com>
2020-09-28 14:13:46 +0200

    made dubelsvenska

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ const button = document.getElementById("add_text");
 const output = document.getElementById("output");
 const input = document.getElementById("input");
 
-function welcome (){
-    output.innerHTML = input.value;
+function dubbleWord (){
+    const userInput = input.value;
+    const formattedInput = userInput.split(" ");
+    
+    let output = "";
+
+    for(i = 0; i <= 100; i++){
+        if (typeof formattedInput[i] !== 'undefined'){
+            output += formattedInput[i];
+            output += " ";
+            output += formattedInput[i];
+            output += " ";    
+        }
+        
+    }
+    
+    console.log(output);
+
 }
 
-button.addEventListener("click", welcome);
+button.addEventListener("click", dubbleWord);
 
